01d3091f92c6a37c505f753a33a148d1bde96f6d
0titanium <38120056+0titanium@users.noreply.github.com>
2021-07-22 21:40:39 +0900

    add client

---
+++ client/src/Config.js
@@ -0,0 +1,6 @@
+// server routes
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "7257ff01287b7b3b178fbb342a40da78";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
