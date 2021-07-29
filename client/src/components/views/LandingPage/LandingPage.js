2856f75fa0eed0adb90aa81e87b1ba79d1623653
0titanium <38120056+0titanium@users.noreply.github.com>
2021-07-29 23:00:40 +0900

    restore boiler-plate components

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../../_actions/user_action";
+import { withRouter } from "react-router-dom";
+
+function LandingPage(props) {
+  const dispatch = useDispatch();
+  const onClickHandler = () => {
+    dispatch(logoutUser()).then((response) => {
+      if (response.payload.logoutSuccess) {
+        props.history.push("/login"); // withRouter 필요
+      } else {
+        alert("Error");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>LogOut</button>
+    </div>
+  );
+}
+
+export default withRouter(LandingPage);
